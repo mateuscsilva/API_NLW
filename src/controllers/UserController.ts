61f6a4bc43d7a3f3937879720a8e06d22023255e
mateus.carvalho <mateuscsilva.1@gmail.com>
2021-02-28 17:51:14 -0300

    data validation in userController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UserRepository } from "../repositories/UserRepository";
+import * as yup from "yup"
 
 class UserController{
     async create(request: Request, response: Response){
         const { name, email } = request.body;
         
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome é obrigatório"),
+            email: yup.string().email("Email incorreto").required("Email é obrigatório")
+        });
+
+        try {
+            await schema.validate(request.body, { abortEarly: false });
+        }catch(err){
+            return response.status(400).json({
+                error: err
+            })
+        }
+
         const usersRepository = getCustomRepository(UserRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
